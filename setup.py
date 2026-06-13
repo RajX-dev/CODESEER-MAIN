b26a0c8c647b21058bf7e3e764c35db95388f789
Raj Shekhar <sraj4090ti@gmail.com>
2026-06-13 07:04:36 +0530

    fix: run requirements.txt installation and setup dependencies in setup.py to resolve import errors in CI

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="n3mo",
     version="1.0.0",
     description="N3MO: The Impact Tracker",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
     entry_points={
-    'console_scripts': [
-        'n3mo=src.cli:main',
-    ],
-},
+        'console_scripts': [
+            'n3mo=src.cli:main',
+        ],
+    },
 )
