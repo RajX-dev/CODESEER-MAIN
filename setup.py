6f3d98bb246fc527f70117b112a103b692a4a27b
Raj Shekhar <sraj4090ti@gmail.com>
2026-06-15 11:21:10 +0530

    chore: bump version to 1.1.0 for SaaS and Marketplace release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="n3mo",
-    version="1.0.9",
+    version="1.1.0",
     description="N3MO: The Impact Tracker",
     long_description=long_description,
     long_description_content_type="text/markdown",
