139166f81a9f991f66e3e233e94cc72058ecad1d
Raj Shekhar <sraj4090ti@gmail.com>
2026-07-14 11:34:05 +0530

    chore: bump version to 2.0.3 to fix MCP registry conflict

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="n3mo",
-    version="2.0.2",
+    version="2.0.3",
     description="N3MO: The Impact Tracker",
     long_description=long_description,
     long_description_content_type="text/markdown",
